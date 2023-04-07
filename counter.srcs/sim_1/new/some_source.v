4c25c6191d7df200b70b9b2f988e67a5807f38db
1dNDN <42769281+1dNDN@users.noreply.github.com>
2023-04-07 20:17:55 +0300

    Initial commit

---
+++ counter.srcs/sim_1/new/some_source.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07.04.2023 20:12:20
+// Design Name: 
+// Module Name: some_source
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module some_source(
+    input clk,
+    input count_in,
+    output count_out
+    );
+endmodule
